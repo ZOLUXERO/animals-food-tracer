1270d04a978db8bc6c49060b17bd98a034cc3158
cmorenop <cmorenop@fgs.co>
2024-12-16 17:12:54 -0500

    feat(city): endpoint

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const subscriber = app.get(DaprService);
   await subscriber.start();
-  await app.listen(3001);
+  await app.listen(3004);
 }
 bootstrap();
